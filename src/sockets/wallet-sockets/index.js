e8c5e718851475170f395d92fdea7bcfb1c750dc
lihkir <likyalpha@gmail.com>
2021-07-16 17:56:17 +0000

    update orderbooks functioanlity

--- src/sockets/wallet-sockets/index.js
+++ src/sockets/wallet-sockets/index.js
@@ -14,57 +14,54 @@ const socketEmitEvents = {
 
 const handleConnection = (io) => {
     return (client) => {
+        const clientOptions = {};
         const ip = client.handshake.headers['x-forwarded-for'] || client.conn.remoteAddress.split(":")[3];
         console.log(`New Client Connected! ID: ${client.id}, IP: ${ip}`);
     
         client.on('disconnect', () => {
             orderBooksService.clearOrderbooksFromIp(ip);
-            io.emit('dataFeed', orderBooksService.allDataByDealers);
             console.log(`Client disconnected. socket ID: ${client.id}, IP: ${ip}`);
         });
-    
+
+        client.on('orderbook-market-filter', (filter) => {
+            clientOptions.orderbookMarketFilter = filter;
+            emitOrderbookData(client, clientOptions.orderbookMarketFilter)
+        });
+
+        client.on('update-orderbook', () => {
+            emitOrderbookData(client, clientOptions.orderbookMarketFilter)
+        });
+
         client.on('dealer-data', (dealerData) => {
+            console.log('new Dealer Data');
             const { tradesData, addressPair } = dealerData;
             orderBooksService.addToDealersData(ip, addressPair, tradesData);
-            io.emit('dataFeed', orderBooksService.allDataByDealers);
+            emitOrderbookData(io, clientOptions.orderbookMarketFilter);
         });
-    
-        // client.on(socketOnEvents.LTC_INSTANT_TRADE, (tradeConf) => handleLTCInstantTrade(tradeConf, client));
-        // client.on(socketOnEvents.TOKEN_TOKEN_TRADE, (tradeConf) => handleTokenTokenTrade(tradeConf, client));
-    
     }
 };
 
-const handleTokenTokenTrade = async (tradeConf, client) => {
-    ['MULTYSIG_DATA', 'RAW_HEX']
-        .forEach((e) => client.removeAllListeners(e));
-    const counterparties = await getCounterParties(tradeConf);
-    if (!counterparties || !counterparties.length) {
-        client.emit(socketEmitEvents.TRADE_REJECTION, 'No Counterperties Founds!');
-        console.log('No Counterparties Found!')
-    } else {
-        const counterparty = counterparties[0];
-        tradeConf.cpPubkey = counterparty.addressObj.publicKey;
-        tradeConf.cpAddress = counterparty.addressObj.address;
-        const counterpartyConnection = `${counterparty.ip}:${counterparty.port}`;
-        tokenTokenTrade(tradeConf, client, counterpartyConnection)
-    }
-}
+const emitOrderbookData = (socket, marketFilter) => {
+    if (!marketFilter) return;
+    const orderbookData = convertDealersData(orderBooksService.allDataByDealers);
+    const filteredData = filterOrderBookData(orderbookData, marketFilter);
+    socket.emit('orderbook-data', filteredData);
+};
 
-const handleLTCInstantTrade = async (tradeConf, client) => {
-    ['MULTYSIG_DATA', 'RAW_HEX']
-        .forEach((e) => client.removeAllListeners(e));
-    const counterparties = await getCounterParties(tradeConf);
-    if (!counterparties || !counterparties.length) {
-        client.emit(socketEmitEvents.TRADE_REJECTION, 'No Counterperties Founds!');
-        console.log('No Counterparties Found!')
-    } else {
-        const counterparty = counterparties[0];
-        tradeConf.cpPubkey = counterparty.addressObj.publicKey;
-        tradeConf.cpAddress = counterparty.addressObj.address;
-        const counterpartyConnection = `${counterparty.ip}:${counterparty.port}`;
-        ltcInstantTrade(tradeConf, client, counterpartyConnection)
-    }
+const filterOrderBookData = (data, filter) => {
+    const { firstId, secondId } = filter;
+    if (!firstId || !secondId) return []; 
+    return data.filter(ob => (
+        (ob.propIdForSale === firstId && ob.propIdDesired === secondId) || 
+        (ob.propIdForSale === secondId && ob.propIdDesired === firstId)
+    ));
+};
+
+const convertDealersData = (rawData) => {
+    const convertedData = [];
+    const d1 = Object.values(rawData);
+    d1.forEach(d => d.forEach(_d => _d.tradesData.forEach(__d => convertedData.push(__d))));
+    return convertedData;
 }
 
 const blockCounter = (io) => {
@@ -86,26 +83,21 @@ const connect = () => {
     const { SOCKET_PORT, socketIoConfig } = config;
     const io = require('socket.io')(SOCKET_PORT, socketIoConfig);
     io.on('connection', handleConnection(io));
-    blockCounter(io)
-    // setInterval(() => {
-    //     const data = orderBooksService.allDataByDealers;
-    //     // const data = { data: 'No news is a good news :)'};
-    //     io.emit('dataFeed', data)
-    // }, 5000);
+    blockCounter(io);
 };
 
-const getCounterParties = (tradeConf) => {
-    return new Promise((res, rej) => {
-        const counterpartiesList = channelManager.getCounterparties(true, (result) => {
-            const { data, error } = result;
-            if (error || !data) {
-                rej('There is problem with listing Counterparties');
-                return console.log(error || 'There is problem with listing Counterparties');
-            }
-            res(data)
-        });
-    });
-}
+// const getCounterParties = (tradeConf) => {
+//     return new Promise((res, rej) => {
+//         const counterpartiesList = channelManager.getCounterparties(true, (result) => {
+//             const { data, error } = result;
+//             if (error || !data) {
+//                 rej('There is problem with listing Counterparties');
+//                 return console.log(error || 'There is problem with listing Counterparties');
+//             }
+//             res(data)
+//         });
+//     });
+// }
 
 module.exports = {
     connect
